d8778bbe81caee8f2de38017c115dfff76eb71a2
Nishad Trivedi <nishad.d.trivedi@gmail.com>
2016-10-10 10:03:57 -0500

    Add other d3 chart examples

--- app.py
+++ app.py
@@ -13,5 +13,25 @@ def graph():
     return render_template('graph.html')
 
 
+@app.route('/bar')
+def bar():
+    return render_template('int_bar.html')
+
+
+@app.route('/line')
+def line():
+    return render_template('int_line.html')
+
+
+@app.route('/filter')
+def filter():
+    return render_template('int_line_filter.html')
+
+
+@app.route('/scatter')
+def scatter():
+    return render_template('int_scatter.html')
+
+
 if __name__ == '__main__':
     app.run(debug=True)
