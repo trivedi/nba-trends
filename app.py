ea0c97d3c10ff7dbdec655f3bf57be5aeeecc107
Nishad Trivedi <nishad.d.trivedi@gmail.com>
2016-10-10 00:55:05 -0500

    Add sample graph plot

--- app.py
+++ app.py
@@ -8,5 +8,10 @@ def index():
     return render_template('index.html')
 
 
+@app.route('/graph')
+def graph():
+    return render_template('graph.html')
+
+
 if __name__ == '__main__':
     app.run(debug=True)
